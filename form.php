cb3b421cb132370e156cfd7e2166a5cf29548cff
flwz <93744561+flwz@users.noreply.github.com>
2023-09-23 12:13:59 +0200

    Update form.php

--- form.php
+++ form.php
@@ -3,22 +3,19 @@ $name = $_POST['namn'];
 $visitor_email = $_POST['email'];
 $message = $_POST['meddelande'];
 
-$email_form = 'info@neexr.com'
+$email_from = 'info@neexr.com';
+$email_meddelande = 'Ny förfrågan';
 
-$email_meddelande = 'Ny förfrågan'
+$email_body = "User Namn: {$name}\n".
+              "User Email: {$visitor_email}\n".
+              "User Meddelande: {$message}\n";
 
-$email_body = "User Namn $namn.\n".
-"User Email $visitor_email.\n".
-"User Meddelande $namn.\n".
+$to = 'hoger.farhan@gmail.com';
 
-$to = 'hoger.farhan@gmail.com'
+$headers = "From: {$email_from}\r\n";
+$headers .= "Reply-To: {$visitor_email}\r\n";
 
-$headers = "From $email_from \r\n";
-
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_meddelande,$email_body,$headers);
+mail($to, $email_meddelande, $email_body, $headers);
 
 header("Location: index.html");
-
 ?>
